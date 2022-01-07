1211796f1c8d8a66fe2bb4942c96f17cf3700b64
Nanda Velugoti <nandshnu@gmail.com>
2022-01-07 17:40:06 -0600

    Initial Commit - Solved Day 1 problem - TODO Day 2 problem

---
+++ 001-sonar-sweep.py
@@ -0,0 +1,29 @@
+def part1(data):
+    count = 0
+    prev = data[0]
+    for x in data:
+        if x > prev:
+            count += 1
+        prev = x
+    print('Part 1 Answer:', count)
+
+def part2(data):
+    count = 0
+    prev = sum(data[:3])
+    for i in range(len(data)):
+        s = sum(data[i:i+3])
+        if s > prev:
+            count += 1
+        prev = s
+    print('Part 2 Answer:', count)
+
+def main():
+    data = []
+    with open('input/001-sonar-sweep.txt') as f:
+        data = [int(x) for x in f.readlines()]
+
+    part1(data)
+    part2(data)
+       
+if __name__ == '__main__':
+    main()
---
+++ 002-dive.py
@@ -0,0 +1,9 @@
+def main():
+    data = []
+    with open('input/002-dive.txt') as f:
+        data = [int(x) for x in f.readlines()]
+    
+    #TODO
+       
+if __name__ == '__main__':
+    main()
