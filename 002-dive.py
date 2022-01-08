ee07bb526800475fbdb169ee35f47455ab29a4a9
Nanda Velugoti <nandshnu@gmail.com>
2022-01-07 18:01:58 -0600

    Solved: Day 2 problem Misc: Add `.gitignore`

--- 002-dive.py
+++ 002-dive.py
@@ -1,9 +1,46 @@
+def part1(data):
+    pos = 0
+    depth = 0
+    for step in data:
+        inst, val = step.split()
+        val = int(val)
+        if (inst == 'forward'):
+            pos += val
+        elif (inst == 'up'):
+            depth -= val
+        elif(inst == 'down'):
+            depth += val
+        else:
+            print('Invalid instruction in data!')
+            exit(1)
+    print("Part 1 Answer:", pos*depth)
+
+def part2(data):
+    pos = 0
+    depth = 0
+    aim = 0
+    for step in data:
+        inst, val = step.split()
+        val = int(val)
+        if (inst == 'forward'):
+            pos += val
+            depth += aim*val
+        elif (inst == 'up'):
+            aim -= val
+        elif(inst == 'down'):
+            aim += val
+        else:
+            print('Invalid instruction in data!')
+            exit(1)
+    print("Part 2 Answer:", pos*depth)
+
 def main():
     data = []
     with open('input/002-dive.txt') as f:
-        data = [int(x) for x in f.readlines()]
+        data = [x for x in f.readlines()]
     
-    #TODO
+    part1(data)
+    part2(data)
        
 if __name__ == '__main__':
     main()
